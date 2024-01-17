12d683f3d428766d7e24a0b5c834ed3ff29b697e
HungNP <21520252@gm.uit.edu.vn>
2024-01-17 17:28:19 +0700

    Add AddProduct screen and update dependencies

--- src/main.tsx
+++ src/main.tsx
@@ -1,10 +1,23 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
-import App from './App.tsx'
-import './index.css'
+import { RouterProvider, createBrowserRouter } from 'react-router-dom';
+import { AddProduct } from './pages/add-product';
+import MiniDrawer from './components/drawer';
+
+
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <MiniDrawer />
+  },
+  {
+    path: "/products/new",
+    element: <AddProduct />
+  }
+]);
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
-    <App />
+    <RouterProvider router={router} />
   </React.StrictMode>,
 )
---
+++ src/pages/add-product.tsx
@@ -0,0 +1,13 @@
+import { Typography } from "@mui/material";
+import MiniDrawer from "../components/drawer";
+
+export function AddProduct() {
+    return (
+        <MiniDrawer >
+            <Typography>
+                Add product screenaaaaaaaaaaaasdfsdf
+            </Typography>
+
+        </MiniDrawer>
+    )
+}
