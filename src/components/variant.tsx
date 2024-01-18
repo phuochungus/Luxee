dadfe6c47bc9e0285d08e3b04bcaa87eba93f00f
HungNP <21520252@gm.uit.edu.vn>
2024-01-18 18:36:33 +0700

    Refactor variant component to improve readability and maintainability

--- src/components/variant.tsx
+++ src/components/variant.tsx
@@ -1,48 +1,102 @@
-import { Box, Breadcrumbs, Button, TextField, Typography } from "@mui/material";
+import {
+    Box,
+    Breadcrumbs,
+    Button,
+    TextField,
+    Typography,
+} from "@mui/material";
 import React from "react";
 
-
-interface VariantProps {
+export interface IVariant {
     name: string;
     values: string[];
 }
 
-export function Variants(props: VariantProps) {
+export function Variants(props: IVariant) {
     const [openEditor, setOpenEditor] = React.useState(true);
 
     const handleOpenEditor = () => {
         setOpenEditor(true);
-    }
+    };
 
     const handleCloseEditor = () => {
         setOpenEditor(false);
-    }
+    };
 
-    return (<>
-        {openEditor ?
-            <>
-                <TextField label="Name" size="small" defaultValue={props.name} required />
-                
-            </>
-            :
-            <Box sx={{ display: "flex", flexDirection: "row", alignContent: "center", justifyContent: "space-between" }}>
-                <Box>
-                    <Typography>{props.name}</Typography>
-                    <Breadcrumbs>
-                        {props.values.map((value, index) =>
-                            <Box key={index}>
-                                <Typography>{value}</Typography>
+    return (
+        <>
+            {openEditor ?
+                <>
+                    <Box sx={{ display: "flex" }}>
+                        <Box>
+
+                            <Typography>Name</Typography>
+                            <TextField
+                                size="small"
+                                defaultValue={props.name}
+                            />
+                        </Box>
+                        <Box sx={{ ml: 3 }}>
+
+                            <Typography>Values</Typography>
+                            <Box
+                                sx={{
+                                    display: "flex",
+                                    flexDirection: "column",
+                                }}
+                            >
+                                {props.values.map((value, index) => (
+                                    <Box>
+                                        <TextField
+                                            sx={{ mb: 1 }}
+                                            key={index}
+                                            size="small"
+                                            defaultValue={value}
+                                        />
+                                    </Box>
+                                ))}
                             </Box>
-                        )}
-                    </Breadcrumbs>
-                </Box>
-                <Box sx={{ display: "flex", alignContent: "center", flexWrap: "wrap" }}>
+                        </Box>
+                    </Box>
+                </>
+                :
+                <Box
+                    sx={{
+                        display: "flex",
+                        flexDirection: "row",
+                        alignContent: "center",
+                        justifyContent: "space-between",
+                    }}
+                >
                     <Box>
-                        <Button variant="outlined" onClick={handleOpenEditor}>Edit</Button>
+                        <Typography>{props.name}</Typography>
+                        <Breadcrumbs>
+                            {props.values.map((value, index) => (
+                                <Box key={index}>
+                                    <Typography>{value}</Typography>
+                                </Box>
+                            ))}
+                        </Breadcrumbs>
+                    </Box>
+                    <Box
+                        sx={{
+                            display: "flex",
+                            alignContent: "center",
+                            flexWrap: "wrap",
+                        }}
+                    >
+                        <Box>
+                            <Button
+                                variant="outlined"
+                                onClick={handleOpenEditor}
+                            >
+                                Edit
+                            </Button>
+                        </Box>
                     </Box>
                 </Box>
-            </Box>
-        }
-    </>
-    )
+
+            }
+        </>
+    );
 }
--- src/pages/add-product.tsx
+++ src/pages/add-product.tsx
@@ -1,79 +1,144 @@
-import { Box, Button, Container, Divider, Grid, IconButton, List, ListItem, TextField, Typography, styled } from "@mui/material";
+import {
+    Box,
+    Button,
+    Container,
+    Divider,
+    Grid,
+    IconButton,
+    List,
+    ListItem,
+    TextField,
+    Typography,
+    styled,
+} from "@mui/material";
 import MiniDrawer from "../components/drawer";
-import AutoAwesomeOutlinedIcon from '@mui/icons-material/AutoAwesomeOutlined';
-import CloudUploadIcon from '@mui/icons-material/CloudUpload';
+import AutoAwesomeOutlinedIcon from "@mui/icons-material/AutoAwesomeOutlined";
+import CloudUploadIcon from "@mui/icons-material/CloudUpload";
 import React from "react";
-import { Variants } from "../components/variant";
+import { IVariant, Variants } from "../components/variant";
 
-const VisuallyHiddenInput = styled('input')({
-    clip: 'rect(0 0 0 0)',
-    clipPath: 'inset(50%)',
+const VisuallyHiddenInput = styled("input")({
+    clip: "rect(0 0 0 0)",
+    clipPath: "inset(50%)",
     height: 1,
-    overflow: 'hidden',
-    position: 'absolute',
+    overflow: "hidden",
+    position: "absolute",
     bottom: 0,
     left: 0,
-    whiteSpace: 'nowrap',
+    whiteSpace: "nowrap",
     width: 1,
 });
 
+interface ICreateProduct {
+    title: string;
+    description: string;
+    media: string[];
+    price: number;
+    compareAt: number;
+    cost?: number;
+    variants: IVariant[];
+}
+
 export function AddProduct() {
-    const [showGenerateText, setShowGenerateText] = React.useState(false)
+    const [showGenerateText, setShowGenerateText] =
+        React.useState(false);
 
     const handleAutoPressed = () => {
-        setShowGenerateText(showGenerateText => !showGenerateText)
-    }
+        setShowGenerateText((showGenerateText) => !showGenerateText);
+    };
 
-    // useEffect(() => { console.log(showGenerateText) }, [showGenerateText])
-
+    const variants: IVariant[] = [
+        {
+            name: "Color",
+            values: ["Red", "Blue", "Green"],
+        },
+        {
+            name: "Size",
+            values: ["S", "M", "L"],
+        },
+    ];
 
     return (
-        <MiniDrawer >
-            <Container sx={{ display: "flex", flexDirection: "column" }}>
+        <MiniDrawer>
+            <Container
+                sx={{ display: "flex", flexDirection: "column" }}
+            >
                 <Typography>Title</Typography>
                 <TextField sx={{ my: 1 }} size="small" />
                 <Typography>Description</Typography>
-                <TextField sx={{ mt: 1 }} multiline size="small" minRows={3} />
-                <Box sx={{
-                    display: "flex",
-                    justifyContent: "flex-end",
-                }}>
+                <TextField
+                    sx={{ mt: 1 }}
+                    multiline
+                    size="small"
+                    minRows={3}
+                />
+                <Box
+                    sx={{
+                        display: "flex",
+                        justifyContent: "flex-end",
+                    }}
+                >
                     <IconButton onClick={handleAutoPressed}>
                         <AutoAwesomeOutlinedIcon />:
                     </IconButton>
                 </Box>
                 <Typography>Media*</Typography>
                 <Box>
-                    <Button component="label" variant="contained" startIcon={<CloudUploadIcon />}>
+                    <Button
+                        component="label"
+                        variant="contained"
+                        startIcon={<CloudUploadIcon />}
+                    >
                         Upload media
-                        <VisuallyHiddenInput type="file" accept="video/*,image/*" multiple />
+                        <VisuallyHiddenInput
+                            type="file"
+                            accept="video/*,image/*"
+                            multiple
+                        />
                     </Button>
                 </Box>
                 <Typography>Pricing</Typography>
                 <Box>
                     <Grid container spacing={2}>
                         <Grid item xs={3} width={"auto"}>
-                            <TextField label="Price" size="small" required />
+                            <TextField
+                                label="Price"
+                                size="small"
+                                required
+                            />
                         </Grid>
                         <Grid item xs={3} width={"auto"}>
-                            <TextField label="Compare-at" size="small" />
+                            <TextField
+                                label="Compare-at"
+                                size="small"
+                            />
                         </Grid>
                         <Grid item xs={4} width={"auto"} />
                         <Grid item xs={3} width={"auto"}>
                             <TextField label="Cost" size="small" />
                         </Grid>
                         <Grid item xs={3} width={"auto"}>
-                            <TextField label="Profit" size="small" defaultValue={"--"} InputProps={{ readOnly: true }} />
+                            <TextField
+                                label="Profit"
+                                size="small"
+                                defaultValue={"--"}
+                                InputProps={{ readOnly: true }}
+                            />
                         </Grid>
                         <Grid item xs={3} width={"auto"}>
-                            <TextField label="Margin" size="small" InputProps={{ readOnly: true }} defaultValue={"--"} />
+                            <TextField
+                                label="Margin"
+                                size="small"
+                                InputProps={{ readOnly: true }}
+                                defaultValue={"--"}
+                            />
                         </Grid>
                     </Grid>
                 </Box>
 
                 <Typography>Inventory</Typography>
                 <Box>
-                    <Grid container spacing={2} columnSpacing={1} >
+                    <Grid container spacing={2} columnSpacing={1}>
                         <Grid item xs={3}>
                             <TextField label="SKU" size="small" />
                         </Grid>
@@ -82,34 +147,52 @@ export function AddProduct() {
                         </Grid>
                         <Grid item xs={6} />
                         <Grid item xs={3}>
-                            <TextField label="Unavailable" size="small" />
+                            <TextField
+                                label="Unavailable"
+                                size="small"
+                            />
                         </Grid>
                         <Grid item xs={3}>
-                            <TextField label="Commited" size="small" />
+                            <TextField
+                                label="Commited"
+                                size="small"
+                            />
                         </Grid>
                         <Grid item xs={3}>
-                            <TextField label="Available" size="small" />
+                            <TextField
+                                label="Available"
+                                size="small"
+                            />
                         </Grid>
                         <Grid item xs={3}>
-                            <TextField label="On-hand" size="small" defaultValue={"--"} InputProps={{ readOnly: true }} />
+                            <TextField
+                                label="On-hand"
+                                size="small"
+                                defaultValue={"--"}
+                                InputProps={{ readOnly: true }}
+                            />
                         </Grid>
                     </Grid>
                 </Box>
                 <Typography>Variants</Typography>
-                <List >
-                    <ListItem >
-                        <Box sx={{ width: "100%" }}>
-                            <Variants name="Color" values={["Red", "Blue", "Green"]} />
-                        </Box>
-                    </ListItem>
-                    <Divider />
-                    {/* <ListItem >
-                        <Box sx={{ width: "100%" }}>
-                            <Variants name="Color" values={["Red", "Blue", "Green"]} />
-                        </Box>
-                    </ListItem> */}
+                <List>
+                    {variants.map((variant, index) => {
+                        return (
+                            <div key={index}>
+                                <Divider />
+                                <ListItem>
+                                    <Box sx={{ width: "100%" }}>
+                                        <Variants
+                                            name={variant.name}
+                                            values={variant.values}
+                                        />
+                                    </Box>
+                                </ListItem>
+                            </div>
+                        );
+                    })}
                 </List>
-            </Container >
-        </MiniDrawer >
-    )
+            </Container>
+        </MiniDrawer>
+    );
 }
