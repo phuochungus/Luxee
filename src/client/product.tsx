e4f8f22a3545d06a3a4d4e503f7fba41d6cb7da8
HungNP <21520252@gm.uit.edu.vn>
2024-01-31 16:25:31 +0700

    Add .env files and update fetchBase function

--- src/client/product.tsx
+++ src/client/product.tsx
@@ -16,9 +16,45 @@ export interface Product {
     available?: Number;
     committed?: Number;
 }
+type Unfetch = (
+    url: string,
+    options?: {
+        method?: string;
+        headers?: Record<string, string>;
+        credentials?: "include" | "omit";
+        body?: Parameters<XMLHttpRequest["send"]>[0];
+    }
+) => Promise<UnfetchResponse>;
+
+type UnfetchResponse = {
+    ok: boolean;
+    statusText: string;
+    status: number;
+    url: string;
+    text: () => Promise<string>;
+    json: () => Promise<any>;
+    blob: () => Promise<Blob>;
+    clone: () => UnfetchResponse;
+    headers: {
+        keys: () => string[];
+        entries: () => Array<[string, string]>;
+        get: (key: string) => string | undefined;
+        has: (key: string) => boolean;
+    };
+};
+
+const fetchBase: Unfetch = (
+    url: string,
+    options?: {
+        method?: string;
+        headers?: Record<string, string>;
+        credentials?: "include" | "omit";
+        body?: Parameters<XMLHttpRequest["send"]>[0];
+    }
+) => fetch(`${import.meta.env.VITE_BACKEND_BASE_UR}${url}`, options);
 
 export const getAllProducts = async () =>
-    fetch("/api/products", {
+    fetchBase("/api/products", {
         method: "GET",
         headers: {
             "Content-Type": "application/json",
@@ -26,13 +62,13 @@ export const getAllProducts = async () =>
     });
 
 export const getProductById = async (id: Number) =>
-    fetch(`/api/products/${id}`, {
+    fetchBase(`/api/products/${id}`, {
         method: "GET",
         headers: { "Content-Type": "application/json" },
     });
 
 export const createProduct = async (product: any) =>
-    fetch("/api/products", {
+    fetchBase("/api/products", {
         method: "POST",
         headers: {
             "Content-Type": "application/json",
--- vite.config.ts
+++ vite.config.ts
@@ -13,9 +13,4 @@ export default defineConfig({
             },
         ],
     },
-    server: {
-        proxy: {
-            "/api": "http://localhost:8080",
-        },
-    },
 });
